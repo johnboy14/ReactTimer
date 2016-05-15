7a51618d8da831451fa5328f4c76b4a0f68877fe
johnervine@hotmail.com <johnervine@hotmail.com>
2016-05-15 13:14:47 +0100

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+var Navigation = (props) => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li className="menu-text">
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li className="menu-text">
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by John Ervine
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
